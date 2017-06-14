03634a9e2530c3ace08d7e2f5fb576231b1b3108
Bruno Coimbra <b-coimbra@hotmail.com>
2017-06-14 16:13:03 -0300

    update

--- shell.rb
+++ shell.rb
@@ -3,8 +3,8 @@
 
 system "title #{$0} - #{Dir.pwd}"
 
-# setting prompt's ansi codes
-BEGIN { trace_var :$Prompt, proc { |c| $> << "\n\e[0;\n\e[33m┌─────┄┄ #{c} \e[33m\e[0m#{Time.now.strftime('%H:%M')}\n\e[33m└──┄\e[0m " }; $Prompt = '' }
+# setting prompt ansi codes
+BEGIN { trace_var :$Prompt, proc { |c| $> << "\n\e[0;\n\e[33m┌─────┄┄ #{c} \e[33m\e[0m#{Time.now.strftime('%H:%M')}\n\e[33m└──┄\e[0m " } }
 
 def main
   # terminate shell if ctrl+c
@@ -14,6 +14,7 @@ def main
     # getting input from the console
     $<.map do |input|
       i = input.to_s.strip
+      $dir = Dir.pwd.split('/')[-1..-1]*?/
 
       # checking if a change of directory was requested
       if i =~ /cd(?<dir>(\s(.*)+))/im
@@ -23,20 +24,31 @@ def main
         if !test(?e, dir)
           $> << "\e[31mNo folder named '#{dir}' in this directory!\e[0m\n"
 
-          $Prompt = ''
+          $Prompt = '' unless has_git?
         else
+          $dir = dir
+
           CMDS["cd"]::(dir) # activates the built-in command to change directory
 
-          $Prompt = "\e[1;35m~/#{dir}\e[0m"
+          $Prompt = "\e[1;35m~/#{dir}\e[0m" unless has_git?
         end
       else
         # if the input was not defined as a built-in command, then try to execute it through the native shell (unless input is blank)
         !CMDS.has_key?(i) ? (system i) : (puts CMDS[i]::()) unless (i.nil? || i.empty? || i[/\r/m]) == true
 
         # changing prompt state to the current directory
-        $Prompt = "\e[1;35m~/#{Dir.pwd.split('/')[-1..-1]*?/}\e[0m"
+        $Prompt = "\e[1;35m~/#{$dir}\e[0m" unless has_git?
       end
     end rescue NoMethodError abort "unknown command"
+  end
+end
+
+# check for git repository
+def has_git?
+  if test(?e, '.git')
+    if (`git rev-parse --git-dir`) =~ /^\.git$/im
+      $Prompt = "#{`git show-branch`[/^\[.*\]/im]} \e[1;35m~/#{$dir}\e[0m"
+    end
   end
 end
 
@@ -60,10 +72,11 @@ CMDS = {
   "pwd"  => -> { Dir.pwd }
 }
 
-# if being executed through ruby, check for --help flag
 case ARGV[0]
-when /(\-+|h)+/i then help
-else main if $0 == __FILE__
+when /(\-+|h)+/i then help # checking for --help flag
+else 
+  $Prompt = '' if !has_git?
+  main if $0 == __FILE__
 end
 
 # checks if shell terminated with an error
