05d8b7cc7be7753bc9ba3892602a8e97c909de92
Bruno Coimbra <b-coimbra@hotmail.com>
2017-07-26 14:10:25 -0300

    update

--- shell.rb
+++ shell.rb
@@ -2,7 +2,9 @@
 # encoding: utf-8
 system "title #{$0}"
 
-$> << "\nwelcome back#{', '+ENV['COMPUTERNAME'].capitalize unless RUBY_PLATFORM[/linux|darwin|mac|solaris|bsd/im]} (´･ω･`)\n"
+define_method(:is_windows) { !RUBY_PLATFORM[/linux|darwin|mac|solaris|bsd/im] }
+
+$> << "\nwelcome back#{', '+ENV['COMPUTERNAME'].capitalize if is_windows} (´･ω･`)\n"
 
 # setting ansi codes
 BEGIN { trace_var :$Prompt, proc { |c| $> << "\n\e[33m┌─────┄┄ #{c} \e[33m\e[0m\e[1;35m░█\e[0m\e[1;46m #{Time.now.strftime('%H:%M')} \e[0m\e[1;35m█░\e[0m\n\e[33m└──┄\e[0m " } }  
@@ -55,17 +57,17 @@ def help
   print %{
     Usage: ruby shell.rb (or run the executable)
 
-    Type any command into the terminal, that's it!
+    Type any command into the terminal, use < to run the previous command, that's it!
   }; exit 0
 end
 
 # Built-in commands
 CMDS = {
-  "cd"      =>-> (dir = ENV['HOME']) { Dir.chdir dir },
+  "clear"   =>-> { system is_windows ? 'cls' : 'clear'; nil },
+  "cd"      =>-> (dir = ENV['HOME']) { Dir.chdir dir; nil },
   "date"    =>-> { Time.now.strftime('%d/%m/%Y') },
   "exit"    =>-> { $> << "bye (￣▽￣)ノ"; exit 0 },
-  "<"       =>-> { system $buffer[-1] },
-  "clear"   =>-> { system 'cls' },
+  "<"       =>-> { CMDS[$buffer[-1]]::() },
   "cmds"    =>-> { CMDS.keys*?| },
   "path"    =>-> { ENV['Path'] },
   "history" =>-> { $buffer*?| },
@@ -74,7 +76,7 @@ CMDS = {
 }
 
 class String
-  define_method(:alias) { |cmd| CMDS.store("#{self}", -> { self[/q/i] ? eval(cmd) : system(cmd) }) }
+  define_method(:alias) { |cmd| CMDS.store("#{self}", -> { self[/q/i] ? eval(cmd) : system(cmd); nil }) }
 end
 
 # Your aliases
