86ce727f28a56f3e74e017ed5f99e090c10aa56c
Bruno Coimbra <b-coimbra@hotmail.com>
2017-10-27 10:02:26 -0200

    build fix

--- src/main.rb
+++ src/main.rb
@@ -10,19 +10,18 @@ Dir[File.join(__dir__, %w[utils], '*.rb')].map(&method(:require))
 # PROMPT
 BEGIN { trace_var :$Prompt, proc { |dir| $> << "\n\e[36m┌──────── #{dir} \e[36m\e[0m\n\e[36m└────\e[0m " } }
 
-###  Command aliases
-##   alias / command / description
+## alias / command / description
 'c'   .alias 'cls', 'clears buffer'
 'q'   .alias 'exit', 'quits shell'
 's'   .alias 'subl .', 'sublime'
 'att' .alias 'git pull', 'update'
 'e'   .alias 'emacs -nw', 'emacs'
 'o'   .alias 'explorer .', 'opens current directory'
-# 'off' .alias 'shutdown -s -f -t 0', 'shutdown'
 
 case ARGV[0]
 when /(\-+h)+/i then help # --help flag
 else
+  exit if defined? Ocra
   # initialize the shell
   Core::new.main if __FILE__ == $0
 end
